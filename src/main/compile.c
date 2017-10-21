35040a0fce8cf4e9172b98fe09f14245587efed2
Wei <drink18@hotmail.com>
2017-10-21 01:08:16 -0400

    * update .gitignore for *.fcode * add preLaunch task for debugging * reformat code

--- src/main/compile.c
+++ src/main/compile.c
@@ -22,126 +22,130 @@
 #include "compile.h"
 #include <string.h>
 // #include <windows.h>
-LinkedList_t wave_file_list=0;
-static String_t Compile_one (String_t file);
+LinkedList_t wave_file_list = 0;
+static String_t Compile_one(String_t file);
 
-static String_t Compile_one (String_t rawfile)
+static String_t Compile_one(String_t rawfile)
 {
-  int pathsize=0,i=0,ret;
-  Pass_t lexAndPass, elaborate,
-    flatten, tacPass, machinePass, x86Pass;
-  Ast_Prog_t ast;
-  Hil_Prog_t hil;
-  Tac_Prog_t tac;
-  Machine_Prog_t machine;
-  Tuple_t tuple;
-  X86_Prog_t x86;
-  //strip the path info
-  String_t filename=String_substr(rawfile,'.');
+    int pathsize = 0, i = 0, ret;
+    Pass_t lexAndPass, elaborate,
+        flatten, tacPass, machinePass, x86Pass;
+    Ast_Prog_t ast;
+    Hil_Prog_t hil;
+    Tac_Prog_t tac;
+    Machine_Prog_t machine;
+    Tuple_t tuple;
+    X86_Prog_t x86;
+    //strip the path info
+    String_t filename = String_substr(rawfile, '.');
 
-  ret = tcc_open(s, rawfile);
-  if (ret < 0) {
-            tcc_error_noabort("file '%s' not found", filename);
+    ret = tcc_open(s, rawfile);
+    if (ret < 0)
+    {
+        tcc_error_noabort("file '%s' not found", filename);
         return ret;
     }
 
+    //init wave file list
+    wave_file_list = LinkedList_new();
+    if (Control_dump_lookup(DUMP_CPP))
+    {
+        /* open the file */
+        String_t cppFileName = genFileName(rawfile, "cpp");
 
+        s->ppfp = fopen(cppFileName, "w");
+        if (!s->ppfp)
+            tcc_error("could not write '%s'", cppFileName);
+        tcc_preprocess(s);
 
-  //init wave file list
-  wave_file_list=LinkedList_new();
-  if (Control_dump_lookup (DUMP_CPP)){
-  	/* open the file */
-	String_t cppFileName=genFileName (rawfile, "cpp");
+        fclose(s->ppfp);
+        return 0;
+    }
+    preprocess_init(s);
+    lexAndPass = Pass_new("lexAndParse",
+                          VERBOSE_SUBPASS,
+                          rawfile,
+                          (Poly_tyId)Parse_parse);
 
-	s->ppfp = fopen(cppFileName, "w");
-    if (!s->ppfp)
-                tcc_error("could not write '%s'", cppFileName);
-	tcc_preprocess(s);
+    ast = Pass_doit(&lexAndPass);
+    tcc_close();
 
-	fclose(s->ppfp);
-     return 0;
-  }
-   preprocess_init(s);
-  lexAndPass = Pass_new ("lexAndParse",
+#ifdef _DEBUG
+    if (Control_dump_lookup(DUMP_AST))
+    {
+        File_saveToFile(genFileName(rawfile, "ast"),
+                        (Poly_tyPrint)Ast_Prog_print, ast);
+    }
+#endif
+    // File_saveToFile (genFileName (rawfile, "c"),
+    //                (Poly_tyPrint)X86_C_Prog_print, ast);
+
+    elaborate = Pass_new("elaboration",
                          VERBOSE_SUBPASS,
-                         rawfile,
-                         (Poly_tyId)Parse_parse);
+                         ast,
+                         (Poly_tyId)Elaborate_main);
+    hil = Pass_doit(&elaborate);
+#ifdef _DEBUG
+    if (Control_dump_lookup(DUMP_HIL))
+    {
+        File_saveToFile(genFileName(rawfile, "hil"),
+                        (Poly_tyPrint)Hil_Prog_print, hil);
+    }
+#endif
+    ast = 0;
 
-  ast = Pass_doit (&lexAndPass);
-  tcc_close();
- 
-  #ifdef _DEBUG
-  if (Control_dump_lookup (DUMP_AST)){
-    File_saveToFile (genFileName (rawfile, "ast"),
-                     (Poly_tyPrint)Ast_Prog_print, ast);
-  }
-  #endif
-     // File_saveToFile (genFileName (rawfile, "c"),
-     //                (Poly_tyPrint)X86_C_Prog_print, ast);
+    flatten = Pass_new("hil",
+                       VERBOSE_SUBPASS,
+                       hil,
+                       (Poly_tyId)Hil_main);
+    tac = Pass_doit(&flatten);
+#ifdef _DEBUG
+    if (Control_dump_lookup(DUMP_TAC))
+    {
+        File_saveToFile(genFileName(rawfile, "tac"),
+                        (Poly_tyPrint)Tac_Prog_print, tac);
+    }
+#endif
+    hil = 0;
 
+    tacPass = Pass_new("tac",
+                       VERBOSE_SUBPASS,
+                       tac,
+                       (Poly_tyId)Tac_main);
+    machine = Pass_doit(&tacPass);
+#ifdef _DEBUG
+    if (Control_dump_lookup(DUMP_MACHINE))
+    {
+        File_saveToFile(genFileName(rawfile, "machine"),
+                        (Poly_tyPrint)Machine_Prog_print,
+                        machine);
+    }
+#endif
 
+    x86Pass = Pass_new("x86",
+                       VERBOSE_SUBPASS,
+                       machine,
+                       (Poly_tyId)X86_main);
+    tuple = Pass_doit(&x86Pass);
+    x86 = Tuple_first(tuple);
+    if (Control_dump_lookup(DUMP_X86))
+    {
+        File_saveToFile(genFileName(rawfile, "s"),
+                        (Poly_tyPrint)Firstech_Prog_print, x86);
+    }
+    if (Control_dump_lookup(DUMP_PAR))
+    {
+        Firstech_Code_gen(x86, filename, 0);
+    }
+    else
+        Firstech_Code_gen(x86, filename, 1);
 
-  elaborate = Pass_new ("elaboration",
-                        VERBOSE_SUBPASS,
-                        ast,
-                        (Poly_tyId)Elaborate_main);
-  hil = Pass_doit (&elaborate);
-  #ifdef _DEBUG
-  if (Control_dump_lookup (DUMP_HIL)){
-    File_saveToFile (genFileName (rawfile, "hil"),
-                     (Poly_tyPrint)Hil_Prog_print, hil);
-  }
-  #endif
-  ast = 0;
-
-  flatten = Pass_new ("hil",
-                      VERBOSE_SUBPASS,
-                      hil,
-                      (Poly_tyId)Hil_main);
-  tac = Pass_doit (&flatten);
-  #ifdef _DEBUG
-  if (Control_dump_lookup (DUMP_TAC)){
-    File_saveToFile (genFileName (rawfile, "tac"),
-                     (Poly_tyPrint)Tac_Prog_print, tac);
-  }
-  #endif
-  hil = 0;
-
-  tacPass = Pass_new ("tac",
-                      VERBOSE_SUBPASS,
-                      tac,
-                      (Poly_tyId)Tac_main);
-  machine = Pass_doit (&tacPass);
-  #ifdef _DEBUG
-  if (Control_dump_lookup (DUMP_MACHINE)){
-    File_saveToFile (genFileName (rawfile, "machine"),
-                     (Poly_tyPrint)Machine_Prog_print,
-                     machine);
-  }
-  #endif
- 
-  x86Pass = Pass_new ("x86",
-                      VERBOSE_SUBPASS,
-                      machine,
-                      (Poly_tyId)X86_main);
-  tuple = Pass_doit (&x86Pass);
-  x86 = Tuple_first (tuple);
-  if (Control_dump_lookup (DUMP_X86)){
-    File_saveToFile (genFileName (rawfile, "s"),
-                     (Poly_tyPrint)Firstech_Prog_print, x86);
-  }
-  if(Control_dump_lookup (DUMP_PAR))
-  	{
-	Firstech_Code_gen(x86,filename,0);
-  	}
-  else  Firstech_Code_gen(x86,filename,1);
-  
-  //add for delete
-  tcc_delete(s);	
-  return Tuple_second (tuple);
+    //add for delete
+    tcc_delete(s);
+    return Tuple_second(tuple);
 }
 
-LinkedList_t Compile_compile (LinkedList_t files)
+LinkedList_t Compile_compile(LinkedList_t files)
 {
-  return LinkedList_map (files, (Poly_tyId)Compile_one);
+    return LinkedList_map(files, (Poly_tyId)Compile_one);
 }
